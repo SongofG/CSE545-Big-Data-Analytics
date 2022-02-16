2965f0c56da2c53d5bf4f1b9f80406c9ed0e6b22
Haein Park <steve121212@naver.com>
2022-02-16 10:17:44 -0500

    Part 1: Streaming

---
+++ streamingCSE545sp22_lastname_id.py
@@ -0,0 +1,124 @@
+##########################################################################
+## streamingCSE545sp22_lastname_id.py  v1
+## 
+## Template code for assignment 1 part 1. 
+## Do not edit anywhere except blocks where a #[TODO]# appears
+##
+## Student Name:
+## Student ID: 
+
+
+import sys
+from pprint import pprint
+from random import random
+from collections import deque
+from sys import getsizeof
+import resource
+
+##########################################################################
+##########################################################################
+# Methods: implement the methods of the assignment below.  
+#
+# Each method gets 1 100 element array for holding ints of floats. 
+# This array is called memory1a, memory1b, or memory1c
+# You may not store anything else outside the scope of the method.
+# "current memory size" printed by main should not exceed 8,000.
+
+MEMORY_SIZE = 100 #do not edit
+
+memory1a =  deque([None] * MEMORY_SIZE, maxlen=MEMORY_SIZE) #do not edit
+
+def task1ADistinctValues(element, returnResult = True):
+    #[TODO]#
+    #procss the element you may only use memory1a, storing at most 100 
+    
+    if returnResult: #when the stream is requesting the current result
+        result = 0
+        #[TODO]#
+        #any additional processing to return the result at this point
+        return result
+    else: #no need to return a result
+        pass
+
+
+memory1b =  deque([None] * MEMORY_SIZE, maxlen=MEMORY_SIZE) #do not edit
+
+def task1BMedian(element, returnResult = True):
+    #[TODO]#
+    #procss the element
+    
+    if returnResult: #when the stream is requesting the current result
+        result = 0
+        #[TODO]#
+        #any additional processing to return the result at this point
+        return result
+    else: #no need to return a result
+        pass
+    
+
+memory1c =  deque([None] * MEMORY_SIZE, maxlen=MEMORY_SIZE) #do not edit
+
+def task1CMostFreqValue(element, returnResult = True):
+    #[TODO]#
+    #procss the element
+    
+    if returnResult: #when the stream is requesting the current result
+        result = 0
+        #[TODO]#
+        #any additional processing to return the result at this point
+        return result
+    else: #no need to return a result
+        pass
+
+
+##########################################################################
+##########################################################################
+# MAIN: the code below setups up the stream and calls your methods
+# Printouts of the results returned will be done every so often
+# DO NOT EDIT BELOW
+
+def getMemorySize(l): #returns sum of all element sizes
+    return sum([getsizeof(e) for e in l])+getsizeof(l)
+
+if __name__ == "__main__": #[Uncomment peices to test]
+    
+    print("\n\nTESTING YOUR CODE\n")
+    
+    ###################
+    ## The main stream loop: 
+    print("\n\n*************************\n Beginning stream input \n*************************\n")
+    filename = sys.argv[1]#the data file to read into a stream
+    printLines = frozenset([10**i for i in range(1, 20)]) #stores lines to print
+    peakMem = 0 #tracks peak memory usage
+    
+    with open(filename, 'r') as infile:
+        i = 0#keeps track of lines read
+        for line in infile:
+        
+            #remove \n and convert to int
+            element = int(line.strip())
+            i += 1
+            
+            #call tasks         
+            if i in printLines: #print status at this point: 
+                result1a = task1ADistinctValues(element, returnResult=True)
+                result1b = task1BMedian(element, returnResult=True)
+                result1c = task1CMostFreqValue(element, returnResult=True)
+                
+                print(" Result at stream element # %d:" % i)
+                print("   1A:     Distinct values: %d" % int(result1a))
+                print("   1B:              Median: %.2f" % float(result1b))
+                print("   1C: Most frequent value: %d" % int(result1c))
+                print(" [current memory sizes: A: %d, B: %d, C: %d]\n" % \
+                    (getMemorySize(memory1a), getMemorySize(memory1b), getMemorySize(memory1c)))
+                
+            else: #just pass for stream processing
+                result1a = task1ADistinctValues(element, False)
+                result1b = task1BMedian(element, False)
+                result1c = task1CMostFreqValue(element, False)
+                
+            memUsage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+            if memUsage > peakMem: peakMem = memUsage
+        
+    print("\n*******************************\n       Stream Terminated \n*******************************")
+    print("(peak memory usage was: ", peakMem, ")")
